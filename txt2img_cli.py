22e9033a835107fe03d5a62140b41ee762333689
hyuri <hyuri.pimentel@gmail.com>
2019-08-19 06:11:28 -0300

    A lot of clean up, removing repetition/improving the design.

--- txt2img_cli.py
+++ txt2img_cli.py
@@ -5,7 +5,6 @@
 By Hyuri Pimentel
 """
 
-
 import argparse
 from pathlib import Path
 
@@ -13,34 +12,28 @@ import txt2img_settings as settings
 from txt2img import text_to_image
 
 
+SETTINGS = settings.settings[settings.active]
+
+
 def main():
-	SETTINGS = settings.settings[settings.active]
+	parser = argparse.ArgumentParser(description="Generates an image file from a string or text file.")
 
-	parser = argparse.ArgumentParser(description="Generate an image out of a piece of text or text file.")
-
+	# Add required arguments
 	parser.add_argument("text", type=str, help="Text or file.")
-	parser.add_argument("-e", "--text_encoding", "-text_encoding", type=str, default=SETTINGS["text_encoding"], help=f"Text encoding. Default is {SETTINGS['text_encoding']}.")
-	parser.add_argument("font_size", type=int, help="Font size.")
-	parser.add_argument("-fg", "--text_color", "-text_color", type=str, default=SETTINGS["text_color"], help=f"Text color. Default os {SETTINGS['text_color']}.")
-	parser.add_argument("-bg", "--background_color", "-background_color", type=str, default=SETTINGS["background_color"], help=f"Background color. Default is {SETTINGS['background_color']}.")
-	parser.add_argument("-cm", "--color_model", "-color_model", type=str, default=SETTINGS["color_model"], help=f"Color model. Default is {SETTINGS['color_model']}.")
-	parser.add_argument("-p", "--padding", "-padding", type=int, default=SETTINGS["padding"], help=f"Padding. Default is {SETTINGS['padding']}.")
-	parser.add_argument("-f", "--font", "-font", type=str, default=SETTINGS["font"], help=f"Font file. Must be TrueType. Default is {SETTINGS['font']}.")
-	parser.add_argument("-r", "--resize", "-resize", type=int, default=SETTINGS["resize"], help=f"Resize image width(in pixels)—height resizes proportionally. Default is {SETTINGS['resize']}.")
-	parser.add_argument("output", type=str, help="Output image file.")
+	parser.add_argument("font_size", type=int, help=f"Font size.")
+	
+	# Add optional arguments
+	for setting in SETTINGS:
+		parser.add_argument(f"--{setting}", f"--{setting}", type=type(SETTINGS[setting]), default=SETTINGS[setting], help=f"{setting.capitalize()}. Default is {SETTINGS[setting]}.")
 
+	# Add required argument (last)
+	parser.add_argument("output", type=Path, help="Output image file.")
+
+	# Parse args
 	args = parser.parse_args()
-	text_to_image(
-		text=args.text,
-		font_size=args.font_size,
-		text_color=args.text_color,
-		background_color=args.background_color,
-		color_model=args.color_model,
-		padding=args.padding,
-		font=args.font,
-		resize=args.resize,
-		output=Path(args.output)
-	)
+
+	# Generate image, passing all parsed args
+	text_to_image(**{key: value for key, value in vars(args).items()})
 
 
 if __name__ == "__main__":
